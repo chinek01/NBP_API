2d978132ad5d3a6f4ab6ac0a3f7822109dbaa1b4
MarcinC <chinek01@gmail.com>
2023-08-02 20:45:15 +0200

    update imports and main urls

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ Author: MC
 
 """
 
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# main urls
+main_url = 'https://api.nbp.pl/api/exchangerates/rates/a/'
+
+usd_url = 'usd/last/100/?format=json'
+chf_url = 'chf/last/100/?format=json'
+chf_eur = 'eur/last/100/?format=json'
+
+
+
 
